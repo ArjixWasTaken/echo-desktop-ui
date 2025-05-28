734f4a48a9cd531790fda10a71c74565f813aca5
ArjixWasTaken <me@arjix.dev>
2025-05-28 22:53:23 +0300

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/solid-router";
+import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
+
+export const Route = createRootRoute({
+	component: () => (
+		<>
+			<div class="p-2 flex gap-2">
+				<Link to="/" class="[&.active]:font-bold">
+					Home
+				</Link>
+			</div>
+			<hr />
+			<Outlet />
+			<TanStackRouterDevtools />
+		</>
+	),
+});
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+import solidPlugin from "vite-plugin-solid";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		TanStackRouterVite({ target: "solid", autoCodeSplitting: true }),
+		solidPlugin(),
+		tailwindcss(),
+	],
+});
